667fa82e997bc6650d92d0779c4cee885dc2574c
Sajjad <sajjad@Sajjads-MacBook-Pro.local>
2025-04-11 19:06:33 +0330

    improve views code base

--- judge/src/internal/serve/views.go
+++ judge/src/internal/serve/views.go
@@ -13,7 +13,25 @@ func problemsetView(c *fiber.Ctx) error {
 	if result.Error != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString("خطا در دریافت اطلاعات کاربر")
 	}
+
 	return render(c, "problemset", fiber.Map{
 		"Title": "CloudiJudge | سوالات",
 	})
+}
+
+func signinView(c *fiber.Ctx) error {
+	return render(c, "signin", fiber.Map{
+		"Title": "CloudiJudge | ورود کاربر",
+	})
+}
+
+func landingView(c *fiber.Ctx) error {
+	return render(c, "landing", fiber.Map{
+		"Title": "CloudiJudge | صفحه اصلی",
+	})
+}
+
+// Template handling function
+func render(c *fiber.Ctx, name string, data interface{}) error {
+	return c.Render("pages/"+name, data, "layouts/main")
 }
